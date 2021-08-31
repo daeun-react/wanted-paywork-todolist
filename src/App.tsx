b0b54b75e496c0b7e4956746483b31d2fce06d41
daeun-react <smile_daeun@naver.com>
2021-08-31 21:20:15 +0900

    feat : App, TodoCreate.tsx 컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,70 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getTodos } from 'reducer/todos/actions';
+import styled from 'styled-components';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from 'reducer';
+import { deleteTodo, getTodos, toggleTodo } from 'reducer/todos/actions';
+import TodoCreate from 'components/TodoCreate';
+import { TodoType } from 'types/todo';
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
+  const { loading, failure, data } = useSelector(
+    (state: RootState) => state.todos,
+  );
 
   useEffect(() => {
     dispatch(getTodos());
   }, [dispatch]);
 
-  return <div>test</div>;
+  // if (loading) return <div>로딩중...</div>;
+  // if (failure) return <div>{'api 호출 실패...'}</div>;
+
+  const onToggle = (todo: TodoType) => {
+    const newTodo: TodoType = { ...todo, isCheck: !todo.isCheck };
+    dispatch(toggleTodo(newTodo));
+  };
+
+  const onDelete = (id: number) => {
+    dispatch(deleteTodo(id));
+  };
+
+  return (
+    <Container>
+      <TodoWrapper>
+        <TodoCreate />
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              <input
+                type="checkbox"
+                checked={item.isCheck}
+                onChange={() => onToggle(item)}
+              />
+              {item.content}
+              <button onClick={() => onDelete(item.id)}>삭제</button>
+            </li>
+          ))}
+        </ul>
+      </TodoWrapper>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.color.indigo};
+`;
+
+const TodoWrapper = styled.div`
+  width: 768px;
+  height: 500px;
+  padding: 24px;
+  border-radius: 12px;
+  background-color: ${({ theme }) => theme.color.white};
+`;
 
 export default App;
--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { AddTodoType } from 'types/todo';
+import { addTodo } from 'reducer/todos/actions';
 
 interface ITodoCreateProps {}
 
 const TodoCreate: React.FC<ITodoCreateProps> = (props) => {
-  return <></>;
+  const dispatch = useDispatch();
+  const [content, setContent] = useState('');
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    const newTodo: AddTodoType = {
+      content,
+      isCheck: false,
+      color: 'red',
+    };
+
+    dispatch(addTodo(newTodo));
+    setContent('');
+  };
+
+  return (
+    <Form>
+      <InputContainer>
+        <TodoInput
+          placeholder="할 일을 입력해주세요."
+          value={content}
+          onChange={handleChange}
+        />
+        <Button onClick={handleSubmit}>+</Button>
+      </InputContainer>
+    </Form>
+  );
 };
+
+const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  align-self: flex-start;
+  font-size: 12px;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 4px;
+  border: 1px solid ${({ theme }) => theme.color.lightGray};
+  border-radius: 5px;
+`;
+
+const TodoInput = styled.input`
+  flex-grow: 1;
+  min-height: 32px;
+  padding: 4px;
+  font-size: 18px;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.color.lightGray};
+  }
+`;
+
+const Button = styled.button`
+  width: 35px;
+  height: 35px;
+  padding: 8px;
+  background-color: ${({ theme }) => theme.color.red};
+  border-radius: 5px;
+  color: ${({ theme }) => theme.color.white};
+  font-size: 16px;
+`;
 
 export default TodoCreate;
