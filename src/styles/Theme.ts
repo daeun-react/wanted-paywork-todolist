daa3ae57f3a48df4cf41680ef11a44e8cca23ff5
daeun-react <smile_daeun@naver.com>
2021-08-31 21:15:16 +0900

    chore : Theme.ts 색상 추가

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,16 +1,18 @@
 export const Theme = {
   color: {
-    red: '#FF622B',
+    red: '#FF485C',
     orange: '#FF8B37',
     yellow: '#F7C73D',
     green: '#35AF63',
     lightRed: '#ffebee',
     lightGreen: '#f8f9e0',
     blue: '#4CB1F8',
+    lightBlue: '#D2DEEA',
     purple: '#A771FE',
     indigo: '#354A60',
     white: '#FFFFFF',
     black: '#2D2D2D',
     gray: '#4B4B4B',
+    lightGray: '#D5D5D5',
   },
 };
