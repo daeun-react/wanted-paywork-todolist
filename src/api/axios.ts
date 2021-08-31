1f89c23f0acf659bcdbd72f99e35e7c5067c8cef
daeun-react <smile_daeun@naver.com>
2021-08-31 21:17:23 +0900

    feat : todo CRUD 관련 axios 함수 생성

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,11 +1,35 @@
 import axios from 'axios';
+import { AddTodoType, TodoType } from 'types/todo';
+import { BASE_URL } from 'utils/constants';
+
+export const axiosCreate = axios.create({
+  baseURL: BASE_URL,
+});
 
 export const getTodoAll = async () => {
-  const response = await axios.get(`/todos`);
+  const response = await axiosCreate.get<TodoType[]>('/todo');
   return response.data;
 };
 
 export const getTodoById = async (id: number) => {
-  const response = await axios.get(`/todos/${id}`);
+  const response = await axiosCreate.get<TodoType>(`/todo/${id}`);
   return response.data;
+};
+
+export const addTodoAPI = async (todo: AddTodoType) => {
+  const response = await axiosCreate.post<TodoType>('/todo', {
+    ...todo,
+    createAt: new Date().toISOString(),
+  });
+  return response.data;
+};
+
+export const toggleTodoAPI = async (todo: TodoType) => {
+  const response = await axiosCreate.put<TodoType>(`/todo/${todo.id}`, todo);
+  return response.data;
+};
+
+export const deleteTodoAPI = async (id: number) => {
+  await axiosCreate.delete(`/todo/${id}`);
+  return id;
 };
