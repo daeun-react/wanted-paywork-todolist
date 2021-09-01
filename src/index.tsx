994695ce59931648de03e12be227b9590d8bed74
daeun-react <smile_daeun@naver.com>
2021-09-02 06:54:53 +0900

    feat : ie11 polyfill 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
