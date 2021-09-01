ff8aa085785b5b77ad0aafe58c74073376a7cb97
daeun-react <smile_daeun@naver.com>
2021-09-02 04:16:07 +0900

    feat : type 및 utils 추가

--- src/types/todo.d.ts
+++ src/types/todo.d.ts
@@ -1,15 +1,25 @@
-type color = 'red' | 'orange' | 'yellow' | 'green' | 'blue';
-
 export type TodoType = {
   id: number;
   content: string;
   isCheck: boolean;
   createAt: string;
-  color: color;
+  color: Color;
+  due: string;
 };
 
 export type AddTodoType = {
   content: string;
   isCheck: boolean;
-  color: color;
+  color: Color;
+  due: string;
+};
+
+export type Color = 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'purple';
+
+export type ObjectStringType = {
+  [key: string]: string;
+};
+
+export type ObjectNumberType = {
+  [key: string]: number;
 };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,59 @@
-export const BASE_URL = 'http://localhost:4000';
+import {
+  ADD_TODO,
+  DELETE_TODO,
+  GET_TODOS,
+  TOGGLE_TODO,
+  UPDATE_TODO,
+} from 'reducer/todos/actions';
+import { ObjectStringType } from 'types/todo';
+
+export const BASE_URL = 'http://paywork-todo.herokuapp.com/';
+// export const BASE_URL = 'http://localhost:4000';
+export const TIME_ZONE: string = 'ko-KR';
+export const DATE_OPTION: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  weekday: 'long',
+};
+
+export const DATE_OPTION_ITEM: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
+
+export const TODO_STATUS: ObjectStringType = {
+  STARTED: 'START',
+  FINISHED: 'FINISH',
+};
+
+export const COLOR_LIST: string[] = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'purple',
+];
+
+export const SORT_LIST: ObjectStringType = {
+  create: '생성순',
+  due: '마감임박순',
+};
+
+export const SUCCESS_MESSAGE: ObjectStringType = {
+  GET_TODOS: 'TodoList 불러오기 성공!',
+  ADD_TODO: 'TodoList 추가 성공!',
+  TOGGLE_TODO: 'TodoList 토글 성공!',
+  DELETE_TODO: 'TodoList 삭제 성공!',
+  UPDATE_TODO: 'TodoList 수정 성공!',
+};
+
+export const ERROR_MESSAGE: ObjectStringType = {
+  [GET_TODOS]: 'TodoList 불러오기 실패!',
+  [ADD_TODO]: 'TodoList 추가 실패!',
+  [TOGGLE_TODO]: 'TodoList 토글 실패!',
+  [DELETE_TODO]: 'TodoList 삭제 실패!',
+  [UPDATE_TODO]: 'TodoList 수정 실패!',
+};
