bcea6ebd152f877830daada25017f64665c559a7
daeun-react <smile_daeun@naver.com>
2021-09-02 04:01:31 +0900

    feat : axios API  수정 및 updateTodoAPI 함수 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { AddTodoType, TodoType } from 'types/todo';
-import { BASE_URL } from 'utils/constants';
+import { BASE_URL, SUCCESS_MESSAGE } from 'utils/constants';
 
 export const axiosCreate = axios.create({
   baseURL: BASE_URL,
@@ -8,28 +8,52 @@ export const axiosCreate = axios.create({
 
 export const getTodoAll = async () => {
   const response = await axiosCreate.get<TodoType[]>('/todo');
-  return response.data;
+  return { msg: SUCCESS_MESSAGE.GET_TODOS, result: response.data };
 };
 
-export const getTodoById = async (id: number) => {
-  const response = await axiosCreate.get<TodoType>(`/todo/${id}`);
-  return response.data;
-};
+// export const getTodoById = async (id: number) => {
+//   const response = await axiosCreate.get<TodoType>(`/todo/${id}`);
+//   return response.data;
+// };
 
 export const addTodoAPI = async (todo: AddTodoType) => {
   const response = await axiosCreate.post<TodoType>('/todo', {
     ...todo,
     createAt: new Date().toISOString(),
   });
-  return response.data;
+  return { msg: SUCCESS_MESSAGE.ADD_TODO, result: response.data };
 };
 
-export const toggleTodoAPI = async (todo: TodoType) => {
-  const response = await axiosCreate.put<TodoType>(`/todo/${todo.id}`, todo);
-  return response.data;
+export const toggleTodoAPI = async (payload: {
+  id: number;
+  isCheck: boolean;
+}) => {
+  await axiosCreate.patch(`/todo/${payload.id}`, {
+    isCheck: payload.isCheck,
+  });
+  return { msg: SUCCESS_MESSAGE.TOGGLE_TODO, result: payload.id };
+};
+
+export const updateTodoAPI = async (payload: {
+  id: number;
+  content: string;
+}) => {
+  await axiosCreate.patch(`/todo/${payload.id}`, {
+    content: payload.content,
+  });
+  return {
+    msg: SUCCESS_MESSAGE.UPDATE_TODO,
+    result: payload.id,
+    content: payload.content,
+  };
 };
 
 export const deleteTodoAPI = async (id: number) => {
   await axiosCreate.delete(`/todo/${id}`);
-  return id;
+  return { msg: SUCCESS_MESSAGE.DELETE_TODO, result: id };
 };
+
+// export const deleteTodoAPI = async (id: number) => {
+//   await axiosCreate.delete(`/todo/${id}`);
+//   return id;
+// };
